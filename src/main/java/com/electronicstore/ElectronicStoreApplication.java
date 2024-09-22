5c541ca2d1ebecd2da7a209d323caa87a3b893b5
Adi <adityabeborta@gmail.com>
2024-09-22 11:58:06 +0530

    added db configs and added modelmapper dependency inside pom.xml

--- src/main/java/com/electronicstore/ElectronicStoreApplication.java
+++ src/main/java/com/electronicstore/ElectronicStoreApplication.java
@@ -1,7 +1,9 @@
 package com.electronicstore;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ElectronicStoreApplication {
@@ -9,5 +11,6 @@ public class ElectronicStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicStoreApplication.class, args);
 	}
+
 
 }
